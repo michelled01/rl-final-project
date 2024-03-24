577e2e85cc4069e016c363e669b86a06032b3c80
Michelle <79888145+michelled01@users.noreply.github.com>
2024-03-24 16:59:56 -0500

    Update main.py

--- README.md
+++ README.md
@@ -9,7 +9,15 @@ Currently testing on Pong (Atari 2600 Game).
 To execute, cd into `rl-project`, install the packages from `requirements.txt`, then run `python main.py`.
 
 # TODO 
-Implement a single agent for playing multiple games. Provides a variation of the meta-controller agent idea presented in Agent57.
+Implement a single agent for playing multiple games. 
+
+Implement a baseline with random action policy (each game a subset of 18 predetermined actions)
+
+Implement a Gym RL model that randomizes the game on `reset()`. [See link](https://gymnasium.farama.org/tutorials/gymnasium_basics/environment_creation). From there, we can identify how to generalize things like the Reward function and state space. 
+
+# Minor TODOS
+For observation space, can preprocess to observe only the 128 Bytes of RAM of the console
 
 # Other Resources
+[Solving 57 Atari Games with a framework Agent57](https://deepmind.google/discover/blog/agent57-outperforming-the-human-atari-benchmark/)
 [Solving Pong with DQN, DDPG, PPO](https://coax.readthedocs.io/en/latest/examples/atari/index.html)
--- main.py
+++ main.py
@@ -1,36 +1,26 @@
 import numpy as np
-import gym
+import pygame
+import gymnasium as gym
+from gymnasium import spaces
 import random
 import matplotlib.pyplot as plt
 import os
 
-def test_pong():
-
-    env = gym.make('ALE/Pong-v5',render_mode='human')
-
-    """Reset our environment, notice it returns the first frame of the game"""
-    first_frame = env.reset()
-
-    """ In Pong the actions are:
-    0 = Stay Still
-    1 = Shoot Ball
-    2 = Move Right
-    3 = Move Left
-    4 = Move Right and Shoot Ball
-    5 = Move Left and Shoot Ball
-    """
-
-    """Now lets take a bunch of random actions and watch the gameplay using render.
-    If the game ends we will reset it using env.reset"""
-
-    for i in range(10000):
-        a = random.sample([0,1,2,3,4,5] , 1)[0]
-        f_p,r,d,_,info = env.step(a)
+def test_random_env():
+    Envs = ['ALE/Pong-v5',"ALE/Breakout-v5","ALE/Centipede-v5","ALE/SpaceInvaders-v5","ALE/Asteroids-v5"]
+    env = gym.make(random.choice(Envs), render_mode='human')
+    env.reset()
+    
+    for i in range(200):
+        print(i)
+        a = env.action_space.sample()
+        f_p,r,d,t,info = env.step(a)
         env.render()
-        if d == True:
+        if d or t:
+            env = gym.make(random.choice(Envs), render_mode='human')
             env.reset()
 
     env.close()
 
 if __name__ == "__main__":
-    test_pong()
+    test_random_env()
