f457c5a9d83c95c1014667b63b756a1f22022b68
pandi <epicnyanpandi@gmail.com>
2024-03-24 17:37:50 -0500

    vectorize

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-import numpy as np
-import pygame
-import gymnasium as gym
-from gymnasium import spaces
 import random
-import matplotlib.pyplot as plt
-import os
 
-def test_random_env():
-    Envs = ['ALE/Pong-v5',"ALE/Breakout-v5","ALE/Centipede-v5","ALE/SpaceInvaders-v5","ALE/Asteroids-v5"]
-    env = gym.make(random.choice(Envs), render_mode='human')
-    env.reset()
-    
-    for i in range(200):
-        print(i)
-        a = env.action_space.sample()
-        f_p,r,d,t,info = env.step(a)
-        env.render()
-        if d or t:
-            env = gym.make(random.choice(Envs), render_mode='human')
-            env.reset()
+import numpy as np
+import gymnasium as gym
 
-    env.close()
+def get_env_specs_ale_all():
+    return [
+        spec for spec in gym.envs.registry.values()
+        if spec.namespace == 'ALE' and spec.kwargs.get('obs_type') == 'rgb']
 
-if __name__ == "__main__":
-    test_random_env()
+def get_env_specs_ale_filtered():
+    def criteria(spec):
+        env = gym.make(spec) # this is slow (needs to load ROM)
+        print(spec.name, env.action_space)
+        return env.action_space == gym.spaces.Discrete(4) # TODO
+    return [spec for spec in get_env_specs_ale_all() if criteria(spec)]
+
+def main():
+    env_ids = ['ALE/Pong-v5','ALE/Breakout-v5','ALE/Centipede-v5','ALE/SpaceInvaders-v5','ALE/Asteroids-v5']
+    # thunk is necessary to capture env_id by value
+    env_thunk = lambda env_id: lambda: gym.make(env_id, full_action_space=True, render_mode='human') # render_mode=None to disable rendering
+    with gym.vector.AsyncVectorEnv([env_thunk(env_id) for env_id in env_ids]) as env:
+        env.reset()
+        for i in range(200):
+            a = env.action_space.sample()
+            obs, reward, terminated, truncated, info = env.step(a)
+            print(reward)
+            #print(f"obs = {obs}\nreward = {reward}\nterminated = {terminated}\ntruncated = {truncated}, info = {info}")
+
+if __name__ == '__main__':
+    from pprint import pprint
+    pprint(get_env_specs_ale_all())
+    main()
