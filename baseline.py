0420779389ae4a3e9ad7ddbad4224f48b290fa3f
pandi <epicnyanpandi@gmail.com>
2024-03-31 18:29:09 -0500

    baseline.py

--- baseline.py
+++ baseline.py
@@ -3,13 +3,14 @@ import stable_baselines3
 import os
 
 def main():
-    baseline_algo = stable_baselines3.A2C # or A2C, try getting DQN to work too
+    baseline_algo = stable_baselines3.PPO # or A2C, try getting DQN to work too
     atari_name = 'Pong'
     model_path = f"baseline_{baseline_algo.__name__}_{atari_name}"
     env = stable_baselines3.common.env_util.make_atari_env(f"ALE/{atari_name}-v5")
+    env = stable_baselines3.common.vec_env.VecFrameStack(env, n_stack=4)
     if not os.path.isfile(f"{model_path}.zip"):
-        model = baseline_algo("MlpPolicy", env, verbose=1)
-        model.learn(total_timesteps=10000, log_interval=4)
+        model = baseline_algo("CnnPolicy", env, verbose=1)
+        model.learn(total_timesteps=20_000, log_interval=4)
         model.save(model_path)
         del model # remove to demonstrate saving and loading
 
