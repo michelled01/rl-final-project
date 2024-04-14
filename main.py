078064b04b788db5102441eb91dfe8777bc159d5
pandi <epicnyanpandi@gmail.com>
2024-04-14 15:42:19 -0500

    choose easier games

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ import gymnasium as gym
 import atari_env
 
 def main():
-    env_ids = ['ALE/Pong-v5','ALE/Breakout-v5','ALE/Centipede-v5','ALE/SpaceInvaders-v5','ALE/Asteroids-v5']
+    env_ids = [
+      'ALE/Boxing-v5', # 1707%
+      'ALE/Breakout-v5', # 1327%
+      # 'ALE/Pong-v5', # 132%
+      # 'ALE/SpaceInvaders-v5', # 121%
+      # 'ALE/Centipede-v5', # 62%
+      # "ALE/Asteroids-v5' # 7%
+    ]
     with atari_env.make_env(env_ids, render_mode='human') as env:
         env.reset()
         for i in range(200):
